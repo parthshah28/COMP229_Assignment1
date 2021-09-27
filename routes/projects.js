15d3ce8c78d474be6611abdd25668a9671d15d6e
Parth Shah <shahp7210@gmail.com>
2021-09-27 11:36:49 +0530

    updated

--- routes/projects.js
+++ routes/projects.js
@@ -1,3 +1,9 @@
+/**
+* @author Parth Shah
+* @studentID 301171805
+* @date September 27, 2021
+* @description Personal Portfolio website
+*/
 /* GET projects page. */
 var express = require("express");
 var router = express.Router();
